e129409941a56031a4fd431414c86f5457f86199
Pierre <kasparianpierre@gmail.com>
2024-12-28 20:15:04 +0100

    vcbn

--- api/index.py
+++ api/index.py
@@ -1,20 +1,6 @@
 from flask import Flask
-# import gradio as gr
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    
-
-    # def to_uppercase(text):
-    #     return text.upper()
-
-    # # Créer l'interface Gradio
-    # iface = gr.Interface(
-    #     fn=to_uppercase,  # La fonction à utiliser
-    #     inputs="text",    # Le type d'entrée
-    #     outputs="text"    # Le type de sortie
-    # )
-    # import threading
-    # threading.Thread(target=iface.launch, kwargs={"server_port": 7860}, daemon=True).start()
-    return "Gradio interface launched"
+    return "<p>Hello, World!</p>"
